dc2074233ab06dba19ec5e6a9edf4129402d7d86
Tsonglew <tsonglew@gmail.com>
2023-03-23 22:23:14 +0800

    Create lottery.py

---
+++ lottery.py
@@ -0,0 +1,10 @@
+class Lottery:
+    def __init__(self, participants):
+        self.participants = participants
+
+    def draw_winner(self):
+        if not self.participants:
+            return None
+        winner = random.choice(self.participants)
+        self.participants.remove(winner)
+        return winner
